b062be9e14159b9d3e0154857b6cbc08241ccb8d
rbackrock <cokekokotea@gmail.com>
2022-10-19 23:34:02 +0800

    # update

--- pages/big-data-screen/3d/controls.js
+++ pages/big-data-screen/3d/controls.js
@@ -4,4 +4,6 @@ import camera from './camera'
 const canvas = document.querySelector('#canvas-3d')
 const controls = new OrbitControls(camera, canvas)
 
+controls.maxPolarAngle = Math.PI / 180 * 90
+
 export default controls
