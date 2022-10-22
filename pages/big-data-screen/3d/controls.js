ba55d87133a59122d24c7ccc592e65ff49274a30
rbackrock <cokekokotea@gmail.com>
2022-10-23 00:03:45 +0800

    # update

--- pages/big-data-screen/3d/controls.js
+++ pages/big-data-screen/3d/controls.js
@@ -6,6 +6,6 @@ import renderer from './renderer'
 
 const controls = new OrbitControls(camera, renderer.domElement)
 // 不能看到仓库底部
-controls.maxPolarAngle = Math.PI / 180 * 90
+controls.maxPolarAngle = Math.PI / 180 * 75
 
 export default controls
