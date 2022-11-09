7859971e479a074b1c56013b8dd98f8b836a007f
rbackrock <cokekokotea@gmail.com>
2022-11-09 23:05:24 +0800

    # update

--- pages/threejs-init/main.js
+++ pages/threejs-init/main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader.js'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -19,6 +21,30 @@ const geometry = new THREE.CylinderGeometry( 1, 1, 1, 32 )
 const material = new THREE.MeshBasicMaterial( {color: 0xffff00} )
 const cylinder = new THREE.Mesh( geometry, material )
 scene.add( cylinder )
+
+const objLoader = new OBJLoader();
+objLoader.load('/models/test/4.obj', (root) => {
+  // console.log(root)
+  root.traverse(child => {
+    if (child.isMesh) {
+      const edges = new THREE.EdgesGeometry(child.geometry)
+      const lines = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: '#1E90FF' }))
+      lines.scale.set(0.003, 0.003, 0.003)
+      scene.add(lines);
+
+      const t1 = new THREE.MeshBasicMaterial({
+        color: 0x1E90FF,
+        opacity: 0.6,
+      transparent: true
+      })
+      const z = new THREE.Mesh(child.geometry, t1)
+      z.scale.set(0.003, 0.003, 0.003)
+      console.log(z)
+      scene.add(z)
+    }
+  })
+  scene.add(root);
+});
 
 function render() {
   renderer.render(scene, camera)
